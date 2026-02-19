23387f400abccd3e714246bfb8512dd3106a442b
anxiousss <saimonkheresh@gmail.com>
2026-02-19 22:04:02 +0300

    WEB-7 | chore(frontend): some work with server

--- web/URLShortener/src/main.js
+++ web/URLShortener/src/main.js
@@ -1,19 +1,17 @@
-const userData = {
-    link: ''
-}
-
-
-fetch('http://server/index.html',  {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    }, body: JSON.stringify(userData)})
-
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
+function send_link() {
+    fetch('http://server/shorten', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }, body: JSON.stringify(document.getElementById('long-url').value)
     })
-    .then(data => console.log('Успех:', data))
-    .catch(error => console.error('Ошибка:', error));
+
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => console.log('Успех:', data))
+        .catch(error => console.error('Ошибка:', error));
+}
