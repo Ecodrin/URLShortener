e9b3eceb0abe99d2060ac5661f7610c9a3cf8dea
anxiousss <saimonkheresh@gmail.com>
2026-02-19 13:08:37 +0300

    WEB-7 | feat(frontend): fetch request wrote

---
+++ web/URLShortener/src/main.js
@@ -0,0 +1,19 @@
+const userData = {
+    link: ''
+}
+
+
+fetch('http://server/index.html',  {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    }, body: JSON.stringify(userData)})
+
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => console.log('Успех:', data))
+    .catch(error => console.error('Ошибка:', error));
