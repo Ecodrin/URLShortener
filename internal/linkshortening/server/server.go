c0f2e7fcc9ca2b2f191bd155653b20bda20a1b24
Ecodrin <matvfo@ya.ru>
2026-03-04 20:58:06 +0300

    WEB-18 | feat(backend): add hash password

--- internal/linkshortening/server/server.go
+++ internal/linkshortening/server/server.go
@@ -18,6 +18,7 @@ import (
 	_ "github.com/lib/pq"
 
 	"github.com/skip2/go-qrcode"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Server struct {
@@ -43,6 +44,16 @@ func IsValidPath(path string) bool {
 		return false
 	}
 	return true
+}
+
+func (server *Server) GetHashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), server.config.hashCost)
+	return string(bytes), err
+}
+
+func (server *Server) CompareHashAndPassword(hash, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
 
 func (server *Server) CreateNewLinkHandler(w http.ResponseWriter, r *http.Request) {
@@ -133,9 +144,16 @@ func (server *Server) RegisterHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	hashPassword, err := server.GetHashPassword(msg.Password)
+	if err != nil {
+		http.Error(w, "internal error", http.StatusInternalServerError)
+		server.logger.Println("error in RegisterHandler server.GetHashPassword: ", err.Error())
+		return
+	}
+
 	succes, err := db.CreateUser(server.DB, handlers.User{
 		Login:    msg.Login,
-		Password: msg.Password,
+		Password: hashPassword,
 	})
 	if err != nil {
 		http.Error(w, "internal error", http.StatusInternalServerError)
@@ -180,10 +198,12 @@ func (server *Server) AuthHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "internal error", http.StatusInternalServerError)
 		return
 	}
-	if user.Password != msg.Password {
+
+	if server.CompareHashAndPassword(user.Password, msg.Password) {
 		http.Error(w, "incorrect login or password", http.StatusBadRequest)
 		return
 	}
+
 	jwtToken, err := handlers.CreateJWTToken(msg.Login, server.config.JWTSecret)
 	if err != nil {
 		http.Error(w, "internal error", http.StatusInternalServerError)
@@ -393,6 +413,9 @@ func StartServer() *Server {
 	var err error
 	server := Server{
 		config: LoadConfig(),
+	}
+	if server.config == nil {
+		panic("error in load config")
 	}
 
 	server.mux = http.NewServeMux()
